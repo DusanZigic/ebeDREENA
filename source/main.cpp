12bf9c4ff29424df80dca2678aba375359db61ad
DusanZigic <zigic.dusan1991@gmail.com>
2024-03-05 12:18:40 +0100

    added class objects

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 #include "ltables.hpp"
+#include "energyloss.hpp"
 
 #include <iostream>
 #include <string>
@@ -21,10 +22,10 @@ int main(int argc, char const *argv[])
 		std::cout << "use: 'calculation' -h for extra help" << std::endl;
 		return 0;
 	}
-    // else if (argv[1] ==  std::string("AverageEL")) {
-    //     if (GetInputs(argc, argv, collsys, sNN, pName, centrality, xB, eventN, BCPP, phiGridN, TIMESTEP, TCRIT, BCPSEED) == 0) return -1;
-    //     AverageEL();
-    // }
+    else if (argv[1] ==  std::string("AverageEL")) {
+        energyLoss EnergyLoss(argc, argv);
+		EnergyLoss.runEnergyLoss();
+    }
     else if (argv[1] == std::string("LTables")) {
 		lTables LTables(argc, argv);
 		LTables.runLTables();
